e17ab30c0592ca16ea925326461d105922de9894
Ankit  Bishnoe <bishnoiankit903@gmail.com>
2025-12-02 09:38:43 +0530

    Delete program_come directory

--- program_come/musiclibrary.py
+++
@@ -1,6 +0,0 @@
-music = {
-    "nijjar":"https://www.youtube.com/watch?v=kNs4-96Netw&list=RDkNs4-96Netw&start_radio=1",
-    "arjan": "https://www.youtube.com/watch?v=UqnxxAA52YQ&list=RDUqnxxAA52YQ&start_radio=1",
-    "karan": "https://www.youtube.com/watch?v=wBw6Leb5F2U&list=RDwBw6Leb5F2U&start_radio=1",
-    "prem": "https://www.youtube.com/watch?v=vH3sX3tJ9wg&list=RDvH3sX3tJ9wg&start_radio=1"
-}
